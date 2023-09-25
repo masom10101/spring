2f05bbd02834e05124dc13d796702bceb5471f86
Masum Sarkar <masum110011@gmail.com>
2023-09-25 22:52:50 +0530

    hm

--- src/main/java/com/example/demo/controllers/DepartmentController.java
+++ src/main/java/com/example/demo/controllers/DepartmentController.java
@@ -1,22 +1,53 @@
 package com.example.demo.controllers;
 
 import com.example.demo.entity.Department;
+import com.example.demo.repository.DeparmentRepository;
+import com.example.demo.service.DeparmentServiceImpl;
 import com.example.demo.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class DepartmentController {
 
 
+   /* @Autowired
+    private DepartmentService departmentService;*/
+
     @Autowired
-    private DepartmentService departmentService;
+    private DeparmentRepository deparmentRepository;
 
 
     @PostMapping(value = "departments")
     public Department save(@RequestBody Department department) {
-        return departmentService.saveDeparment(department);
+//       return departmentService.saveDeparment(department);
+
+
+        return deparmentRepository.save(department);
+
+
+//        return "Department saved";
     }
+
+
+    @GetMapping(value = "departments/{Id}")
+    public Department getDepartmentById(@PathVariable("Id") Long id) {
+//       return departmentService.saveDeparment(department);
+
+        return deparmentRepository.findById(id).get();
+
+
+//        return "Department saved";
+    }
+
+   /* String sayNello(String b){
+        return b+"Yay";
+    }
+
+    // "pari"
+    String sayHello(String a){
+       String aa= sayNello(a)+"Bay";
+       return  aa;
+    }*/
+
 }
